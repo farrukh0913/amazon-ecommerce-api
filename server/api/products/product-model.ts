cc5eb494e4f649a194a6b91590e971f02c953633
mubich1 <mubshirnaseer24@gmail.com>
2024-03-14 19:19:12 +0500

    minor

--- server/api/products/product-model.ts
+++ server/api/products/product-model.ts
@@ -3,7 +3,7 @@ import { Model, Schema, model } from 'mongoose';
 let product:Schema = new Schema({
     productName: { type:String, required:false, trim:true},
     price: { type:String, required:false, trim:true},
-    discription: { type:String, required:false, trim:true},
+    description: { type:String, required:false, trim:true},
     image: { type:String, required:false, trim:false}
 })
 
